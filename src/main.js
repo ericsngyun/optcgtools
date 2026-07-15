412c05ff37c644800926a2015ecf65514872137d
Eric Yun <111465862+ericsngyun@users.noreply.github.com>
2026-07-15 15:22:13 -0700

    add Svelte entrypoint

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import App from "./App.svelte";
+import "../public/css/cards.css";
+import "../public/css/cards/base.css";
+import "../public/css/cards/one-piece-sp.css";
+import "../public/css/cards/one-piece-alt-art.css";
+import "./site.css";
+
+const app = new App({
+  target: document.getElementById("app")
+});
+
+export default app;
