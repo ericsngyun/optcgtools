c52bac6532c359452322e5e62bf7381022e5a1fe
Eric Yun <111465862+ericsngyun@users.noreply.github.com>
2026-07-15 16:47:11 -0700

    load reference comparison styles

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "../public/css/cards/base.css";
 import "../public/css/cards/one-piece-sp.css";
 import "../public/css/cards/one-piece-alt-art.css";
 import "./site.css";
+import "./research.css";
 
 const app = new App({
   target: document.getElementById("app")
